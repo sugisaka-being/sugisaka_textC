accfc599fb422b9a768e4f6e304f6e5bcee2f3c2
杉坂 麗葵(SUGISAKA Reina) <sugisaka@beingcorp.co.jp>
2024-10-18 10:31:30 +0900

    #517156 [C#テキスト]3章演習問題
    3章演習問題の解答を作成しました。

---
+++ Chapter3/Chapter3-1-1/Program3-1-1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chapter3_1_1 {
+    internal class Program {
+        /* [問題3-1-1]
+        　　以下のリストが定義してあります。
+        　　var numbers = new List<int> {12, 87, 94, 14, 53, 20, 40, 35,  76, 91, 31, 17, 48 };
+        　　このリストに対して、ラムダ式を使い、次のコードを書いてください。
+
+        　　1.
+        　　List<T>のExistsメソッドを使い、8か9で割り切れる数があるかどうかを調べ、その結果をコンソールに出力してください。
+
+        　　2.
+        　　List<T>のForEachメソッドを使い、各要素を2.0で割った値をコンソールに出力してください。
+
+        　　3.
+        　　LINQのWhereメソッドを使い、値が50以上の要素を列挙し、その結果をコンソールに出力してください。
+
+        　　4.
+        　　LINQのSelectメソッドを使い、それぞれの値を2倍にし、その結果をList<int>に格納してください。
+        　　その後、List<int>の各要素をコンソールに出力してください。
+        */
+        static void Main(string[] args) {
+            var wNumbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            // 1.
+            Console.WriteLine(wNumbers.Exists(n => n % 8 == 0 || n % 9 == 0));
+
+            // 2.
+            wNumbers.ForEach(n => Console.WriteLine(n / 2.0));
+
+            // 3.
+            var wNumbers1 = wNumbers.Where(n => n >= 50);
+            foreach (var wN in wNumbers1) {
+                Console.WriteLine(wN);
+            }
+
+            // 4.
+            var wNumbers2 = wNumbers.Select(n => n * 2).ToList();
+            foreach (int wN in wNumbers2) {
+                Console.WriteLine(wN);
+            }
+        }
+    }
+}
---
+++ Chapter3/Chapter3-1-2/Program3-1-2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chapter3_1_2 {
+    internal class Program {
+        /* [問題3-1-2]
+        　　以下のリストが定義してあります。
+        　　var names = new List<string> {
+            　　"Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canbera", "Hong Kong",
+        　　};
+        　　このリストに対して、ラムダ式を使い、次のコードを書いてください。
+
+        　　1.
+        　　コンソールから入力した都市名が何番目に格納されているかList<T>のFindIndexメソッドを使って調べ、その結果をコンソールに出力してください。
+        　　見つからなかったら、-1を出力してください。なお、コンソールからの入力には、Console.ReadLineメソッドを利用してください。
+        　　var line = Console.ReadLine();
+
+        　　2.
+        　　LINQのCountメソッドを使い、小文字の'o'が含まれている都市名がいくつあるかカウントし、その結果をコンソールに出力してください。
+
+        　　3.
+        　　LINQのWhereメソッドを使い、小文字の'o'が含まれている都市名を抽出し、配列に格納してください。
+        　　その後、配列の各要素をコンソールに出力してください。
+
+        　　4.
+        　　LINQのWhereメソッドとSelectメソッドを使い、'B'で始まる都市名の文字数を抽出し、その文字数をコンソールに出力してください。
+        　　都市名を表示する必要はありません。
+        */
+        static void Main(string[] args) {
+            var wCityNames = new List<string> {
+                "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canbera", "Hong Kong",
+            };
+
+            // 1.
+            Console.WriteLine("都市名を入力してください。");
+            var wLine = Console.ReadLine();
+            Console.WriteLine($"{wLine}は{wCityNames.FindIndex(n => n == wLine)}番目に格納されている。");
+
+            // 2.
+            Console.WriteLine($"'o'が含まれている都市名は{wCityNames.Count(n => n.Contains('o'))}つある。");
+
+            // 3.
+            Console.WriteLine("'o'が含まれている都市名を以下に記述する。");
+            var wCityNames_o = wCityNames.Where(n => n.Contains('o')).ToArray();
+            foreach (var wCityName in wCityNames_o) {
+                Console.WriteLine(wCityName);
+            }
+
+            // 4.
+            Console.WriteLine("'B'で始まる都市名の文字数を以下に記述する。");
+            var wCityNames_B = wCityNames.Where(n => n[0] == 'B').Select(n => n.Length).ToArray();
+            foreach (var wCityName in wCityNames_B) {
+                Console.WriteLine(wCityName);
+            }
+        }
+    }
+}
