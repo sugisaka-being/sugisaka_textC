2356f64655840d280999bb941b688d5a940072d7
杉坂 麗葵(SUGISAKA Reina) <sugisaka@beingcorp.co.jp>
2024-10-25 14:22:34 +0900

    #517165 [C#テキスト]5章演習問題
    コーディング規約に則り、コードを修正いたしました。

--- Chapter5/Chapter5-1-4/Program5-1-4.cs
+++ Chapter5/Chapter5-1-4/Program5-1-4.cs
@@ -15,7 +15,7 @@ namespace Chapter5_1_4 {
             Console.WriteLine($"作家　: {wNovelInfos[0]}{Environment.NewLine}代表作: {wNovelInfos[1]}{Environment.NewLine}誕生年: {wNovelInfos[2]}");
 
             // 追加課題
-            var wAddNovelInfos = wNovel.Split(';').Select(x => x.Split('=')).ToDictionary(Infos => Infos[0], Infos => Infos[1]);
+            var wAddNovelInfos = wNovel.Split(';').Select(x => x.Split('=')).ToDictionary(x => x[0], x => x[1]);
             Console.WriteLine($"作家　: {wAddNovelInfos["Novelist"]}{Environment.NewLine}代表作: {wAddNovelInfos["BestWork"]}{Environment.NewLine}誕生年: {wAddNovelInfos["Born"]}");
         }
     }
