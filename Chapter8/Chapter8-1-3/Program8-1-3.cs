5cabdee7deeb74f6ce116de90b1ef4e35da61ee6
杉坂 麗葵(SUGISAKA Reina) <sugisaka@beingcorp.co.jp>
2024-10-30 15:23:17 +0900

    #517172 [C#テキスト]8章演習問題
    8章演習問題の解答を作成いたしました。

---
+++ Chapter8/Chapter8-1-3/Program8-1-3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Chapter8_1_3 {
+    internal class Program {
+        /* [問題8-1-3]
+        　　ある処理時間を計測するTimeWatchクラスを定義してください。
+        　　TimeWatchの使い方を以下に示します。
+        
+        　　var tw = new TimeWatch();
+        　　tw.Start();
+        　　//処理
+        　　TimeSpan duration = tw.Stop();
+        　　Console.WriteLine("処理時間は{0}ミリ秒でした",duration.TotalMilliseconds);
+        */
+        static void Main(string[] args) {
+            var tw = new TimeWatch();
+            tw.Start();
+            //処理
+            TimeSpan wDuration = tw.Stop();
+            Console.WriteLine("処理時間は{0}ミリ秒でした", wDuration.TotalMilliseconds);
+        }
+    }
+}
