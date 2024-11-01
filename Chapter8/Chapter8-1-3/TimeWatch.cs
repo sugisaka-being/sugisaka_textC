f98f54dc4baf056771f17bd17b79ee0415385758
杉坂 麗葵(SUGISAKA Reina) <sugisaka@beingcorp.co.jp>
2024-11-01 13:36:47 +0900

    #517172 [C#テキスト]8章演習問題
    いただいたレビューを元にコードを修正いたしました。

--- Chapter8/Chapter8-1-3/TimeWatch.cs
+++ Chapter8/Chapter8-1-3/TimeWatch.cs
@@ -2,15 +2,15 @@
 
 namespace Chapter8_1_3 {
     internal class TimeWatch {
-        private DateTime Memorytime;
+        private DateTime FMemorytime;
         /// <summary>
         /// 開始時間を記録するメソッド
         /// </summary>
-        public void Start() => this.Memorytime = DateTime.Now;
+        public void Start() => this.FMemorytime = DateTime.Now;
         /// <summary>
         /// 停止時間と開始時間の差を算出するメソッド
         /// </summary>
         /// <returns>開始時間からの経過時間</returns>
-        public TimeSpan Stop() => DateTime.Now - this.Memorytime;
+        public TimeSpan Stop() => DateTime.Now - this.FMemorytime;
     }
 }
