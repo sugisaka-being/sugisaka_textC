c162114819a399f69570388e9a2d4e853e27d94f
杉坂 麗葵(SUGISAKA Reina) <sugisaka@beingcorp.co.jp>
2024-11-08 11:43:14 +0900

    #517179 [C#テキスト]11章演習問題
    11章演習問題の解答を作成いたしました。

---
+++ Chapter11/Chapter11-1-1/Program11-1-1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Chapter11_1_1 {
+    internal class Program {
+        /* [問題11-1-1]
+        　　次のXMLファイルがあります。
+        　　このXMLファイルに対して、1.～4.のコードを書いてください。
+
+        　　<?xml version="1.0" encoding="utf-8" ?>
+        　　<ballSports>
+        　　    <ballsport>
+        　　        <name kanji="籠球">バスケットボール</name>
+        　　        <teammembers>5</teammembers>
+        　　        <firstplayed>1891</firstplayed>
+        　　    </ballsport>
+        　　    <ballsport>
+        　　        <name kanji="排球">バレーボール</name>
+        　　        <teammembers>6</teammembers>
+        　　        <firstplayed>1895</firstplayed>
+        　　    </ballsport>
+        　　    <ballsport>
+        　　        <name kanji="野球">ベースボール</name>
+        　　        <teammembers>9</teammembers>
+        　　        <firstplayed>1846</firstplayed>
+        　　    </ballsport>
+        　　</ballSports>
+
+        　　1.
+        　　XMLファイルを読み込み、競技名とチームメンバー数の一覧を表示してください。
+
+        　　2.
+        　　最初にプレーされた年の若い順に漢字の競技名を表示してください。
+
+        　　3.
+        　　メンバー人数が最も多い競技名を表示してください。
+
+        　　4.
+        　　サッカーの情報を追加して、新たなXMLファイルに出力してください。ファイル名は特に問いません。
+        　　なお、サッカーの情報はご自身で調べてください。手間を惜しまずに調べることもプログラマーにとっては必要なことです。
+        */
+        static void Main(string[] args) {
+            // 1.
+            var wSportsFile = @"..\..\Sample11-1-1.xml";
+            if (!File.Exists(wSportsFile)) {
+                Console.WriteLine("指定されたファイルが存在しませんでした。");
+                return;
+            }
+            var wSportsDocumement = XDocument.Load(wSportsFile);
+            var wSportsElements = wSportsDocumement.Root.Elements();
+            foreach (var wSportsElement in wSportsElements) {
+                Console.WriteLine($"競技名：{wSportsElement.Element("name").Value}　チームメンバー数：{wSportsElement.Element("teammembers").Value}");
+            }
+
+            // 2.
+            var wOrderYoungSports = wSportsElements.OrderBy(x => (int)x.Element("firstplayed"));
+            foreach (var wOrderYoungSport in wOrderYoungSports) {
+                Console.WriteLine($"競技名：{wOrderYoungSport.Element("name").Attribute("kanji").Value}　最初にプレーされた年：{wOrderYoungSport.Element("firstplayed").Value}年");
+            }
+
+            // 3.
+            var wMaxMemberSport = wSportsElements.OrderByDescending(x => int.Parse(x.Element("teammembers").Value)).First();
+            Console.WriteLine($"競技名：{wMaxMemberSport.Element("name").Value}　メンバー人数：{wMaxMemberSport.Element("teammembers").Value}人");
+
+            // 4.
+            var wSoccerDocument = new XElement("ballsport",
+                new XElement("name", "サッカー", new XAttribute("kanji", "蹴球")),
+                new XElement("teammembers", "11"),
+                new XElement("firstplayed", "1863")
+            );
+            wSportsDocumement.Root.Add(wSoccerDocument);
+            wSportsDocumement.Save(@"..\..\NewSample11-1-1.xml");
+        }
+    }
+}
+
---
+++ Chapter11/Chapter11-1-2/Program11-1-2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Chapter11_1_2 {
+    internal class Program {
+        /* [問題11-1-2]
+        　　次のようなXMLファイルがあります。
+
+        　　<?xml version="1.0" encoding="utf-8" ?>
+        　　<difficultkanji>
+        　　    <word>
+        　　        <kanji>鬼灯</kanji>
+        　　        <yomi>ほおずき</yomi>
+        　　    </word>
+        　　    <word>
+        　　        <kanji>暖簾</kanji>
+        　　        <yomi>のれん</yomi>
+        　　    </word>
+        　　    <word>
+        　　        <kanji>杜撰</kanji>
+        　　        <yomi>ずさん</yomi>
+        　　    </word>
+        　　    <word>
+        　　        <kanji>坩堝</kanji>
+        　　        <yomi>るつぼ</yomi>
+        　　    </word>
+        　　</difficultkanji>
+
+        　　このXMLファイルを次の形式に変換し、別のファイルに保存してください。
+
+        　　<?xml version="1.0" encoding="utf-8" ?>
+        　　<difficultkanji>
+        　　    <word kanji="鬼灯" yomi="ほおずき" />
+        　　    <word kanji="暖簾" yomi="のれん" />
+        　　    <word kanji="杜撰" yomi="ずさん" />
+        　　    <word kanji="坩堝" yomi="るつぼ" />
+        　　</difficultkanji>
+        */
+        static void Main(string[] args) {
+            var wWordsFile = @"..\..\Sample11-1-2.xml";
+            if (!File.Exists(wWordsFile)) {
+                Console.WriteLine("指定されたファイルが存在しませんでした。");
+                return;
+            }
+            var wWordsDocument = XDocument.Load(wWordsFile);
+            var wNewWordsElements = wWordsDocument.Root.Elements()
+                .Select(x => new XElement("word",
+                    new XAttribute("kanji", x.Element("kanji").Value),
+                    new XAttribute("yomi", x.Element("yomi").Value)
+                ));
+            var wWordsRoot = new XElement("difficultkanji", wNewWordsElements);
+            wWordsRoot.Save(@"..\..\NewSample11-1-2.xml");
+        }
+    }
+}
