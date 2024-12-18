ff2107b478e41103d176839d8e162b5436dd5b18
sugisaka <sugisaka@beingcorp.co.jp>
2024-12-18 11:39:23 +0900

    #517188 [C#テキスト]16章演習問題
    いただいたレビューをもとに演習問題の解答を修正いたしました。

--- Chapter16/Chapter16-1-2/ParallelFileProcessor.cs
+++ Chapter16/Chapter16-1-2/ParallelFileProcessor.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 
 namespace Chapter16_1_2 {
@@ -15,32 +13,7 @@ namespace Chapter16_1_2 {
         /// <param name="vAllCsFilePaths">C#ソースファイルのファイルパス一覧/param>
         /// <returns>非同期処理の完了を示すタスク</returns>
         public async Task ProcessFilesAsync(string[] vAllCsFilePaths) {
-            await Task.WhenAll(vAllCsFilePaths.Select(x => Task.Run(() => SearchCsFiles(x))));
-        }
-
-        /// <summary>
-        /// 「async」と「await」の両方を含むファイルを検索するメソッド
-        /// </summary>
-        /// <param name="vCsFile">C#のソースファイル</param>
-        static void SearchCsFiles(string vCsFile) {
-            try {
-                var wCsFileContents = File.ReadAllText(vCsFile);
-                if (wCsFileContents.Contains("async") && wCsFileContents.Contains("await")) {
-                    Console.WriteLine(Path.GetFullPath(vCsFile));
-                }
-            }
-            catch (UnauthorizedAccessException wEx) {
-                Console.WriteLine($"ファイルへのアクセスが拒否されました：{wEx.Message}");
-            }
-            catch (PathTooLongException wEx) {
-                Console.WriteLine($"ファイルパスが長すぎます：{wEx.Message}");
-            }
-            catch (OutOfMemoryException wEx) {
-                Console.WriteLine($"メモリが不足しています：{wEx.Message}");
-            }
-            catch (Exception wEx) {
-                Console.WriteLine($"エラーが発生しました：{wEx.Message}");
-            }
+            await Task.WhenAll(vAllCsFilePaths.Select(x => Task.Run(() => CsFileSearcher.SearchCsFiles(x))));
         }
     }
 }
--- Chapter16/Chapter16-1-2/SequentialFileProcessor.cs
+++ Chapter16/Chapter16-1-2/SequentialFileProcessor.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.IO;
-
-namespace Chapter16_1_2 {
+﻿namespace Chapter16_1_2 {
     /// <summary>
     /// 非並列処理を行うロジックをまとめたクラス
     /// </summary>
@@ -13,31 +10,7 @@ namespace Chapter16_1_2 {
         /// <param name="vAllCsFilesPaths">C#ソースファイルのファイルパス一覧</param>
         public void ProcessFiles(string[] vAllCsFilesPaths) {
             foreach (string wCsFile in vAllCsFilesPaths) {
-                SearchCsFiles(wCsFile);
-            }
-        }
-        /// <summary>
-        /// 「async」と「await」の両方を含むファイルを検索するメソッド
-        /// </summary>
-        /// <param name="vCsFile">C#のソースファイル</param>
-        static void SearchCsFiles(string vCsFile) {
-            try {
-                var wCsFileContents = File.ReadAllText(vCsFile);
-                if (wCsFileContents.Contains("async") && wCsFileContents.Contains("await")) {
-                    Console.WriteLine(Path.GetFullPath(vCsFile));
-                }
-            }
-            catch (UnauthorizedAccessException wEx) {
-                Console.WriteLine($"ファイルへのアクセスが拒否されました：{wEx.Message}");
-            }
-            catch (PathTooLongException wEx) {
-                Console.WriteLine($"ファイルパスが長すぎます：{wEx.Message}");
-            }
-            catch (OutOfMemoryException wEx) {
-                Console.WriteLine($"メモリが不足しています：{wEx.Message}");
-            }
-            catch (Exception wEx) {
-                Console.WriteLine($"エラーが発生しました：{wEx.Message}");
+                CsFileSearcher.SearchCsFiles(wCsFile);
             }
         }
     }
